6bc289ea7a04e11882477eaee96237555d46e4da
elirop <leerop.lr@gmail.com>
2022-11-03 05:07:42 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,9 +6,10 @@
 #include "main.h"
 
 /**
- * _print_rev recursion - Prints a string in reverse.
+ * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s)

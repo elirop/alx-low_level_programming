0f164243878b0b4c1f461c68e995865db1116d1b
elirop <leerop.lr@gmail.com>
2022-11-03 05:18:43 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Reverse the length of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.

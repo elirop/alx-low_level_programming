71581da7691a75f65442c0a299d60ff2b91c67fc
elirop <leerop.lr@gmail.com>
2022-11-03 08:32:19 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,10 +21,10 @@ int find_sqrt(int num, int root)
 	if ((root * root) == num)
 		return (root);
 
-	if((root == num/2)
-		return(-1);
+	if ((root == num / 2)
+		return (-1);
 
-	return (find_sqrt(num,root + 1));
+	return (find_sqrt(num, root + 1));
 }
 
 /**
@@ -37,15 +37,13 @@ int find_sqrt(int num, int root)
 
 int _sqrt_recursion(int n)
 {
-     int root = 0;
+int root = 0;
 
-     if(n < 0)
-          return(-1);
+if (n < 0)
+return (-1)
 
-     if(n == 1)
-          return(1);
+if (n == 1)
+return (1);
 
-      return (find_sqrt(n, root));
-}      
-
-
+return (find_sqrt(n, root));
+}
